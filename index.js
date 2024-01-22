2c5167d736ff6b1be2c9bd66588f9da4102701e1
Swetha Janardhana <SwethaJanardhana85@gmail.com>
2024-01-22 11:53:37 +0100

    Task Completed

--- index.js
+++ index.js
@@ -3,16 +3,32 @@ import { Country } from "./components/Country/Country.js";
 
 const container = document.querySelector('[data-js="card-container"]');
 const queryInput = document.querySelector('[data-js="query-input"]');
-
+/* 
 queryInput.addEventListener("input", (event) => {
   container.innerHTML = "";
 
   const searchString = event.target.value;
 
-  const foundCountry = null;
+  const foundCountry = countries.find((country) =>
+    country.name.startsWith(searchString)
+  );
 
   if (foundCountry) {
     const countryElement = Country(foundCountry);
     container.append(countryElement);
   }
+}); */
+queryInput.addEventListener("input", (event) => {
+  container.innerHTML = "";
+
+  const searchString = event.target.value;
+
+  const foundCountries = countries.filter((country) =>
+    country.name.startsWith(searchString)
+  );
+
+  foundCountries.forEach((foundCountry) => {
+    const countryElement = Country(foundCountry);
+    container.append(countryElement);
+  });
 });
